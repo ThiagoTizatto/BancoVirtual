e8f033f6854840ea443611c78060162705e5617c
Thiago Tizatto Stefen <thiagots@ciandt.com>
2026-09-22 16:43:50 -0300

    feat: scaffold da solucao com projetos e referencias

---
+++ tests/Api.Testes/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Testes;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Dominio.Testes/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Dominio.Testes;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
